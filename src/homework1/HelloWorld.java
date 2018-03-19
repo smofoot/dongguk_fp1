8cd7a07b6ebcd0f78371750c35cdd9b4065043ed
smofoot <smofoot@naver.com>
2018-03-19 14:29:26 +0900

    homework1

--- src/homework1/HelloWorld.java
+++ src/homework1/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
      public  static void main(String...args) {
           //print HelloWorld
          /* hahahaha*/
-          System.out.println("Hello World");
-         System.out.println("Hello Java");
+          System.out.println("Hello World!");
+          System.out.println("Hello Java!!");
    }
 }
